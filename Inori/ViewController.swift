aaec4443d660f575e58642dddbac3b1b459cdfa4
sillyplus <oi_boy@sina.cn>
2015-12-04 09:15:32 +0800

    init commit

---
+++ Inori/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inori
+//
+//  Created by silly on 12/4/15.
+//  Copyright © 2015 silly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
